518463e294db333d489b5c0f7fa81ce965cd6d7a
featherbear <andrew.j.wong@outlook.com>
2018-08-29 01:52:28 +1000

    Indent provided code

--- Assignments/ass01/components_asm/pre/1.globals.s
+++ Assignments/ass01/components_asm/pre/1.globals.s
@@ -1,27 +1,28 @@
-############################################################ -*- asm -*-
-# COMP1521 18s2 -- Assignment 1 -- Scrolling Text!
-# Scroll letters from a message in argv[1]
-#
-# Base code by Jashank Jeremy
-# Tweaked by John Shepherd
-# $Revision: 1.5 $
-#
-# Edit me with 8-column tabs!
 
-# Requires:
-#  - `all_chars', defined in chars.s
+                                        ############################################################ -*- asm -*-
+                                        # COMP1521 18s2 -- Assignment 1 -- Scrolling Text!
+                                        # Scroll letters from a message in argv[1]
+                                        #
+                                        # Base code by Jashank Jeremy
+                                        # Tweaked by John Shepherd
+                                        # $Revision: 1.5 $
+                                        #
+                                        # Edit me with 8-column tabs!
 
-# Provides:
-	.globl	main # :: int, [char *], [char *] -> int
-#	.globl	setUpDisplay # :: int, int -> void
-#	.globl	showDisplay # :: void -> void
-	.globl	delay # :: int -> vovid
-	.globl	isUpper # :: char -> int
-	.globl	isLower # :: char -> int
+                                        # Requires:
+                                        #  - `all_chars', defined in chars.s
 
-	.globl	CHRSIZE
-	.globl	NROWS
-	.globl	NDCOLS
-	.globl	MAXCHARS
-	.globl	NSCOLS
-	.globl	CLEAR
+                                        # Provides:
+                                            .globl	main # :: int, [char *], [char *] -> int
+                                        #	.globl	setUpDisplay # :: int, int -> void
+                                        #	.globl	showDisplay # :: void -> void
+                                            .globl	delay # :: int -> vovid
+                                            .globl	isUpper # :: char -> int
+                                            .globl	isLower # :: char -> int
+
+                                            .globl	CHRSIZE
+                                            .globl	NROWS
+                                            .globl	NDCOLS
+                                            .globl	MAXCHARS
+                                            .globl	NSCOLS
+                                            .globl	CLEAR
