aa127b8291654091f882949d6663befee968672c
featherbear <andrew.j.wong@outlook.com>
2018-08-29 15:38:01 +1000

    Update documentation for `display_show`

--- Assignments/ass01/components_asm/display_show.s
+++ Assignments/ass01/components_asm/display_show.s
@@ -4,14 +4,15 @@
 # info: Prints out the contents of `display` (9x80 character matrix)
 #       Function uses syscall 4 (print string) rather than syscall 11 (print char) as it is more efficient
 #
-# vars: a0 - address position in `display`
-#       t0 - temp
+# vars: a0 - temp
+#       t0 - address position in `display`
+#       t1 - temp
 #       t7 - loop counter
 #
 	.text
 display_show:
-    la $a0, display     # a0 = &display     # address position in `display`
-    li $t7, 0           # t7 = 0            # loop counter
+    la $a0, display         # a0 = &display     # address position in `display`
+    li $t7, 0               # t7 = 0            # loop counter
 
     # Loop
     display_show_loop:
@@ -23,26 +24,26 @@ display_show:
         addi $t0, $a0, 80
 
         # Store the byte at the first column, and change it to a null terminator (\0)
-        lb $t1, ($t0)   # t1 = *(t0)
-        sb $0, ($t0)    # *(t0) = '\0'      # Strings end with \0. eg. { 'H', 'i', '\0' }
+        lb $t1, ($t0)       # t1 = *(t0)
+        sb $0, ($t0)        # *(t0) = '\0'      # Strings end with \0. eg. { 'H', 'i', '\0' }
 
         # Print the row
-        li $v0, 4       # syscall 4 -> print string
+        li $v0, 4           # syscall 4 -> print string
         syscall
         nop
 
         # Restore the value of the byte that we changed into a null terminator
-        sb $t1, ($t0)   # *(t0) = t1
+        sb $t1, ($t0)       # *(t0) = t1
 
         # Print out a new-line character
         ## Note: Couldn't have replaced the first columns with a new line instead of a null terminator
         ##       because then the first column would have been lost (Need a 9x81 matrix instead of a 9x80)
-        li $a0, 10      # 0x10 is the new line character
-        li $v0, 11      # syscall 4 -> print character
+        li $a0, 10          # 0x10 is the new line character
+        li $v0, 11          # syscall 4 -> print character
         syscall
         nop
 
-        move $a0, $t0   # Restore a0 as the address position in the display matrix
+        move $a0, $t0       # Restore a0 as the address position in the display matrix
 
         # Go to the next row
         addi $t7, $t7, 1    # t7++      # Increment the row counter
