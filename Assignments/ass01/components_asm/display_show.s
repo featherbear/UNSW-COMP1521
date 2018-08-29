47a1e430fba191581b0b87ae8287b4b6d29b4338
featherbear <andrew.j.wong@outlook.com>
2018-08-29 15:22:23 +1000

    Revert code, all characters of the matrix need to be shown, the all-in-one-syscall-method lost the first columns of each row

--- Assignments/ass01/components_asm/display_show.s
+++ Assignments/ass01/components_asm/display_show.s
@@ -11,43 +11,69 @@
 	.text
 display_show:
     la $a0, display     # a0 = &display     # address position in `display`
-    li $v0, 13          # v0 = new line character
+    li $t7, 0           # t7 = 0            # loop counter
 
-    lb $t0, 80($a0)
-    lb $t1, 160($a0)
-    lb $t2, 240($a0)
-    lb $t3, 320($a0)
-    lb $t4, 400($a0)
-    lb $t5, 480($a0)
-    lb $t6, 560($a0)
-    lb $t7, 640($a0)
-    lb $t8, 720($a0)
-    lb $t9, 721($a0)
+    # Loop
+    display_show_loop:
+        beq $t7, 9, display_show_loopEnd
+        nop
 
-    sb $t0, 80($a0)
-    sb $t0, 160($a0)
-    sb $t0, 240($a0)
-    sb $t0, 320($a0)
-    sb $t0, 400($a0)
-    sb $t0, 480($a0)
-    sb $t0, 560($a0)
-    sb $t0, 640($a0)
-    sb $0, 720($a0)
-    #sb $0,  721($a0)
+        addi $t0, $a0, 80
 
-    li $v0, 4
-    syscall
+        lb $t1, ($t0)
+        sb $0, ($t0)
 
-    sb $t0, 80($a0)
-    sb $t1, 160($a0)
-    sb $t2, 240($a0)
-    sb $t3, 320($a0)
-    sb $t4, 400($a0)
-    sb $t5, 480($a0)
-    sb $t6, 560($a0)
-    sb $t7, 640($a0)
-    sb $t8, 720($a0)
-#    sb $t9, 721($a0)
+        li $v0, 4
+        syscall
+        nop
+        
+        sb $t1, ($t0)
 
-    jr	$ra
-    nop
+        li $a0, 10
+        li $v0, 11
+        syscall
+        nops
+
+        move $a0, $t0
+
+        addi $t7, $t7, 1
+        j display_show_loop
+        nop
+
+    # End loop
+    display_show_loopEnd:
+        jr	$ra
+        nop
+
+### OLD VERSION - Uses syscall 11 (much slower!) ##
+#	.text
+#display_show:
+#    # Method: Perform modulo operation to check for new lines
+#    li $t0, 0 # counter
+#    li $t2, 80
+#    la $t4, display
+#
+#    display_show_loop:
+#        beq $t0, 720, display_show_loopEnd
+#
+#        li $v0, 11
+#        div $t0, $t2
+#
+#        mfhi $t3
+#        bgtz $t3, display_show_loop_notEndLine
+#
+#        li $a0, 10 # \n
+#        syscall
+#
+#        display_show_loop_notEndLine:
+#            li $v0, 11
+#            lb $a0, ($t4)
+#            syscall
+#
+#        addi $t0, $t0, 1
+#        addi $t4, $t4, 1
+#        j display_show_loop
+#
+#    display_show_loopEnd:
+#        jr	$ra
+#        nop
