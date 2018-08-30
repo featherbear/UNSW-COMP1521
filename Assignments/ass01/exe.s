caa40b8325bc7f8a568ae931c412495640de41f8
featherbear <andrew.j.wong@outlook.com>
2018-08-30 19:20:50 +1000

    Wait did I not compile the final version...?

--- Assignments/ass01/exe.s
+++ Assignments/ass01/exe.s
@@ -855,13 +855,13 @@ main_theLength_ge_1:
         # while (s0 < s1) { ... }
         beq $s0, $s1, display_loopEnd
 
-        # Clear the screen
-        jal clearScreen
-        nop
-
         # Populate the contents of `display` from `bigString` starting at the column offset
         move $a0, $s0       # a0 = s0       # a0 is the starting column offset
         jal display_populate
+        nop
+
+        # Clear the screen
+        jal clearScreen
         nop
 
         # Show the display
@@ -1275,36 +1275,49 @@ display_populate:
 # info: Prints out the contents of `display` (9x80 character matrix)
 #       Function uses syscall 4 (print string) rather than syscall 11 (print char) as it is more efficient
 #
-# vars: a0 - address position in `display`
-#       t0 - temp
+# vars: a0 - temp
+#       t0 - address position in `display`
+#       t1 - temp
 #       t7 - loop counter
 #
 	.text
 display_show:
-    la $a0, display     # a0 = &display     # address position in `display`
-    li $t7, 0           # t7 = 0            # loop counter
+    la $a0, display         # a0 = &display     # address position in `display`
+    li $t7, 0               # t7 = 0            # loop counter
 
     # Loop
     display_show_loop:
+        # while (t7 < 9 ) { ... }
         beq $t7, 9, display_show_loopEnd
         nop
 
+        # Go to the start of the next row
         addi $t0, $a0, 80
 
-        lb $t1, ($t0)
-        sb $0, ($t0)
-        li $v0, 4
+        # Store the byte at the first column, and change it to a null terminator (\0)
+        lb $t1, ($t0)       # t1 = *(t0)
+        sb $0, ($t0)        # *(t0) = '\0'      # Strings end with \0. eg. { 'H', 'i', '\0' }
+
+        # Print the row
+        li $v0, 4           # syscall 4 -> print string
         syscall
         nop
-        sb $t1, ($t0)
 
-        li $a0, 10
-        li $v0, 11
+        # Restore the value of the byte that we changed into a null terminator
+        sb $t1, ($t0)       # *(t0) = t1
+
+        # Print out a new-line character
+        ## Note: Couldn't have replaced the first columns with a new line instead of a null terminator
+        ##       because then the first column would have been lost (Need a 9x81 matrix instead of a 9x80)
+        li $a0, 10          # 0x10 is the new line character
+        li $v0, 11          # syscall 4 -> print character
         syscall
+        nop
 
-        move $a0, $t0
+        move $a0, $t0       # Restore a0 as the address position in the display matrix
 
-        addi $t7, $t7, 1
+        # Go to the next row
+        addi $t7, $t7, 1    # t7++      # Increment the row counter
         j display_show_loop
         nop
 
