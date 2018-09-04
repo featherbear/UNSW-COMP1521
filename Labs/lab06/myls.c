3ab915160923c4da245cb2a282f85e36f373fbb8
featherbear <andrew.j.wong@outlook.com>
2018-09-04 10:16:13 +1000

    Add sorting and symlink path resolution to lab06:myls.c

--- Labs/lab06/myls.c
+++ Labs/lab06/myls.c
@@ -15,12 +15,26 @@
 #define MAXFNAME   200
 #define MAXNAME    20
 
+
 char *rwxmode(mode_t, char *);
 char *username(uid_t, char *);
 char *groupname(gid_t, char *);
 
-
 char objType(mode_t st_mode);
+
+int comp(const void* S1, const void* S2) {
+  char* s1 = (char*)S1;
+  char* s2 = (char*)S2;
+  for (int i = 0; i < MAXFNAME; i++) {
+    if (s1[i] == '\0' || s2[i] == '\0') return 0;
+
+    if (s1[i] == s2[i]) continue;
+    if (s1[i] > s2[i]) return 1;
+    return -1;
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -43,34 +57,52 @@ int main(int argc, char *argv[])
    if ((info.st_mode & S_IFMT) != S_IFDIR)
       { fprintf(stderr, "%s: Not a directory\n",argv[0]); exit(EXIT_FAILURE); }
 
+
+
+   char filenames[999][MAXFNAME] = {{'\0'}};
    // open the directory to start reading
    DIR *df; // UNCOMMENT this line
    df = opendir(dirname);
    if (df == NULL) {
-        perror("Error");                
+        perror("Error");
         exit(EXIT_FAILURE);
    }
-   
+
    // read directory entries
    struct dirent *entry; // UNCOMMENT this line
    struct stat file;
+
+   int i = 0;
    while ((entry = readdir(df))) {
        if (entry->d_name[0] == '.') continue;
+       strcpy(filenames[i++], entry->d_name);
+   }
 
+   qsort(filenames, sizeof(filenames)/sizeof(*filenames), sizeof(*filenames), comp);
+
+   for (int j = 0; j < i; j++) {
        char fp[MAXFNAME];
        strcpy(fp, dirname);
        strcat(fp, "/");
-       strcat(fp, entry->d_name);
+       strcat(fp, filenames[j]);
+       // strcat(fp, entry->d_name);
 
        lstat(fp, &file); // use lstat and not stat to view the actual symlink file
 
-       printf("%c%s  %-8.8s %-8.8s %8lld  %s\n",     
-               objType(file.st_mode),
+       char objectType = objType(file.st_mode);
+       // struct stat_ file_;
+       char realLocation[999] = {'\0'};
+       if (objectType == 'l') realpath(filenames[j], realLocation);
+       printf("%c%s  %-8.8s %-8.8s %8lld  %s%s%s\n",
+               objectType,
                  rwxmode(file.st_mode, mode),
                    username(file.st_uid, uname),
                           groupname(file.st_gid, gname),
                                  (long long) file.st_size,
-                                         entry->d_name);
+                                         // entry->d_name);
+                                         filenames[j],
+                                         (objectType == 'l') ? " -> " : "",
+                                         (objectType == 'l') ? realLocation : "");
    }
 
    // finish up
@@ -80,7 +112,7 @@ int main(int argc, char *argv[])
 
 char objType(mode_t st_mode) {
     switch (st_mode & S_IFMT) {
-        case S_IFDIR: 
+        case S_IFDIR:
             return 'd';
         case S_IFREG:
             return '-';
@@ -130,3 +162,4 @@ char *groupname(gid_t gid, char *name)
       snprintf(name, MAXNAME, "%s", ginfo->gr_name);
    return name;
 }
+
