daa6cdbedff9b76a1f9aa83e83aa9fd701ecdaea
featherbear <andrew.j.wong@outlook.com>
2018-09-16 20:24:34 +1000

    quickies

--- quickies/fork.c
+++ quickies/fork.c
@@ -1,4 +1,23 @@
-//
-// Created by Andrew on 5/09/2018.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+int main(void)
+{
+    pid_t pid;
+    wait(NULL);
+
+    pid = fork();
+    if (pid != 0) {
+        wait(NULL);
+        printf("I am the parent.\n");
+
+    }
+    else {
+        printf("I am the child.\n");
+        exit(0);
+    }
+    return 0;
+}
--- quickies/getchar putchar vs read write.c
+++ quickies/getchar putchar vs read write.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+
 int main() {
-    while (read(0, &ch, 1) != 0)
-    write(1, &ch, 1);
+    char ch;
+    while (read(0, &ch, 1) != 0) write(1, &ch, 1);
+    // while ((ch = getchar()) != EOF) putchar(ch);
 }
