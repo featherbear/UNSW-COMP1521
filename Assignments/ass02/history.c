3d29b0ac2c6fc0a82b8a86b6f94a47efdcb0a20f
featherbear <andrew.j.wong@outlook.com>
2018-09-17 23:56:28 +1000

    Documentation + Conform code to reference program's output behaviour.
    Note that the `.mymysh_history` file structure is different, so we get garbage if trying to use it across the two version

--- Assignments/ass02/history.c
+++ Assignments/ass02/history.c
@@ -19,6 +19,17 @@
 
 #define HISTFILE ".mymysh_history"
 
+// I got rid of HistoryEntry!!!
+
+/*
+ * So, it's pretty important to explain what's happening I guess...
+ * This implementation keeps only the last 20 commands, so the recall only goes from !1 - !HISTSIZE   (1 based)
+ * This is achieved by using a modulo `n % m` which will give a number between  -(m-1) and (m-1)
+ * We want to get a number between 0 and m-1 so we can modify the expression to `(n + m) % m`
+ *
+ * This makes the numbering cyclical... 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 ... (Where m = 10)
+ */
+
 typedef struct _history_list {
     int nEntries;
     char commands[MAXHIST][MAXSTR];
@@ -26,11 +37,10 @@ typedef struct _history_list {
 } HistoryList;
 
 HistoryList CommandHistory;
-
-FILE *file;
 char *HISTPATH;
 
 // strip: remove leading/trailing spaces and newline characters from a string
+// 'inspired' by trim() from mymysh.c :)
 void strip(char *str) {
     int first, last;
     first = 0;
@@ -45,24 +55,26 @@ void strip(char *str) {
 // initCommandHistory()
 // - initialise the data structure
 // - read from HISTFILE if it exists
-
 int initCommandHistory() {
+    // Initialise the values of `CommandHistory`
     CommandHistory.nEntries = 0;
     CommandHistory.position = 0;
 
+    // HISTPATH = $HOME/HISTFILE
     char *HOME = getenv("HOME");
     HISTPATH = malloc(strlen(HOME) + 1 + strlen(HISTFILE) + 1);
     sprintf(HISTPATH, "%s/%s", HOME, HISTFILE);
 
+    // Read each line in the file
+    FILE *file;
     if ((file = fopen(HISTPATH, "r"))) {
         char cmd[MAXSTR];
         while (fgets(cmd, MAXSTR, file)) {
             strip(cmd);
             addToCommandHistory(cmd);
         }
-
         fclose(file);
-        return 0;
+        return CommandHistory.position;
     }
     return -1;
 }
@@ -70,26 +82,28 @@ int initCommandHistory() {
 // addToCommandHistory()
 // - add a command line to the history list
 // - overwrite oldest entry if buffer is full
-
 void addToCommandHistory(char *cmdLine) {
-    // cmdLine is at most 200 chars long
     int i = CommandHistory.position;
 
-    if (*(CommandHistory.commands[i]) != '\0') CommandHistory.nEntries--;
+    // If current entry is empty, increase the entry count
+    if (CommandHistory.commands[i][0] == '\0') CommandHistory.nEntries++;
+
+    // Copy the command into the current entry
     strcpy(CommandHistory.commands[i], cmdLine);
 
-    CommandHistory.nEntries++;
-
+    // Update current position index
     CommandHistory.position = (i + 1) % MAXHIST;
 }
 
 // showCommandHistory()
 // - display the list of commands in the history
-
 void showCommandHistory(void) {
+    // Get the first entry
     int offsetOne = CommandHistory.position - CommandHistory.nEntries;
+
+    // Print out each past command
     for (int i = 0; i < CommandHistory.nEntries; i++) {
-        int j = (offsetOne + i + MAXHIST) % MAXHIST;
+        int j = (offsetOne + i + MAXHIST) % MAXHIST; // Get index from 0 - MAXHIST-1
         printf(" %3d %s\n", i + 1, CommandHistory.commands[j]);
     }
 }
@@ -97,13 +111,16 @@ void showCommandHistory(void) {
 // getCommandFromHistory()
 // - get the command line for specified command
 // - returns NULL if no command with this number
-
 char *getCommandFromHistory(int cmdNo) {
+    // Check for indexes that are out of range
     if (cmdNo < 0 || cmdNo >= MAXHIST) return NULL;
 
+    // Get index from 0 - MAXHIST-1
     int offsetOne = CommandHistory.position - CommandHistory.nEntries;
     int j = (offsetOne + cmdNo + MAXHIST) % MAXHIST;
-    if (CommandHistory.commands[j] != '\0') return CommandHistory.commands[j];
+
+    // Return the command if an entry exists
+    if (CommandHistory.commands[j][0] != '\0') return CommandHistory.commands[j];
 
     return NULL;
 }
@@ -111,29 +128,29 @@ char *getCommandFromHistory(int cmdNo) {
 // getLastCommandFromHistory()
 // - get the last command line
 // - returns NULL if no previous command
-
 char *getLastCommandFromHistory() {
     return getCommandFromHistory((CommandHistory.position - 1 + MAXHIST) % MAXHIST);
 }
 
 // saveCommandHistory()
 // - write history to $HOME/.mymysh_history
-
 void saveCommandHistory() {
+    // Get the first entry
     int offsetOne = CommandHistory.position - CommandHistory.nEntries;
 
+    // Write each command, starting from the first entry
+    FILE *file;
     if ((file = fopen(HISTPATH, "w"))) {
         for (int i = 0; i < CommandHistory.nEntries; i++) {
             int j = (offsetOne + i + MAXHIST) % MAXHIST;
-            fputs(CommandHistory.commands[j], file);
-            fputc('\n', file);
+            fprintf(file, "%s\n", CommandHistory.commands[j]);
         }
+        fclose(file);
     }
 }
 
 // cleanCommandHistory
 // - release all data allocated to command history
-
 void cleanCommandHistory() {
     free(HISTPATH);
 }
--- Assignments/ass02/mymysh.c
+++ Assignments/ass02/mymysh.c
@@ -83,125 +83,148 @@ int main(int argc, char *argv[], char *envp[]) {
 
     // initialise command history
     // - use content of ~/.mymysh_history file if it exists
-
-    if (initCommandHistory() == -1); // THEN ERROR
+    initCommandHistory();
 
     // main loop: print prompt, read line, execute command
-
     char line[MAXLINE];
     int tokenLength = 0;
     char **tokens;
-    for (; prompt(), fgets(line, MAXLINE, stdin) != NULL; tokenLength = 0, (*line != '\0' && (freeTokens(tokens), 1))) {
+    int tokenised = 0;
 
-        trim(line); // remove leading/trailing space
-        if (*line != '\0') {
+    for (; prompt(), fgets(line, MAXLINE, stdin) != NULL;
+           tokenLength = 0, (tokenised && (freeTokens(tokens), 1)), tokenised = 0) {
+        // For each loop, print out the prompt and request input
+        // At the end of each loop, set `tokenLength` and `tokenised` to 0.
+        //   Additionally, call `freeTokens(tokens)` if `tokenised` was previously 1.
+        //   (tokenised && (..., 1)) is a hack to use the void function `freeTokens` in an expression
 
-            if (line[0] == '!') {
-                char *recallPtr;
-                if (line[1] == '!' && line[2] == '\0') {
-                    if ((recallPtr = getLastCommandFromHistory())) {
-                        strcpy(line, recallPtr);
-                        printf("Recalling: %s\n", line);
-                    } else {
-                        printf("No command to recall.\n");
-                        continue;
-                    }
-                } else {
-                    if ((recallPtr = getCommandFromHistory(atoi(line + 1) - 1))) {
-                        strcpy(line, recallPtr);
-                        printf("Recalling: %s\n", line);
-                    } else {
-                        printf("Invalid recall!\n");
-                        continue;
-                    }
-                }
-            }
+        // remove leading/trailing space
+        trim(line);
 
-            tokens = tokenise(line, " ");
-            if (strContains(line, "*?[~")) {
-                tokens = fileNameExpand(tokens);
-            }
+        // An empty line was entered, ignore
+        if (*line == '\0') continue;
 
-            while (tokens[++tokenLength] != NULL);
-
-            /* Built-in */
-            if (strcmp(tokens[0], "exit") == 0) {
-                freeTokens(tokens);
-                break;
-            } else if (strcmp(tokens[0], "h") == 0 || strcmp(tokens[0], "history") == 0) {
-                addToCommandHistory(line);
+        // Recall command assumed
+        if (line[0] == '!') {
+            if (line[1] == '\0') {
+                // Show the command history if the input is just `!`
                 showCommandHistory();
-            } else if (strcmp(tokens[0], "pwd") == 0) {
-                printPWD();
-                addToCommandHistory(line);
                 continue;
-            } else if (strcmp(tokens[0], "cd") == 0) {
-                if (tokens[1] == NULL) {
-                    printf("Supply a directory to change into.\n");
-                } else if (chdir(tokens[1]) == 0) {
-                    addToCommandHistory(line);
-                    printPWD();
-                } else {
-                    printf("Cannot cd into %s!\n", tokens[1]);
-                }
-                continue;
+            }
+            char *recallPtr;
+            int cmdNo = 1;
+
+            // If `!!` -> getLastCommandFromHistory
+            // Else `!n` -> getCommandFromHistory(n-1)
+            recallPtr = (line[1] == '!' && line[2] == '\0') ? getLastCommandFromHistory()
+                                                            : getCommandFromHistory((cmdNo = atoi(line + 1)) - 1);
+
+            // Check if the command was found
+            if (recallPtr) {
+                // Command found, so copy into `line`.
+                strcpy(line, recallPtr);
+                printf("%s\n", line);
             } else {
-                char *exec;
-                if ((exec = findExecutable(tokens[0], path))) {
-                    int stdin = STDIN_FILENO;
-                    int stdout = STDOUT_FILENO;
+                printf("No command #%d\n", cmdNo);
+                continue;
+            }
+        }
 
+        /* TOKENISER */
+        tokens = tokenise(line, " "); // Tokenise input line
+        if (strContains(line, "*?[~")) tokens = fileNameExpand(tokens); // Resolve wildcards
+        tokenised = 1; // Flag for freeTokens()
+        while (tokens[++tokenLength] != NULL); // Count the number of tokens
 
-                    if (strContains(tokens[tokenLength - 1], "<>") || strContains(tokens[0], "<>")) {
-                        printf("Bad redirection.\n");
+        // Validate redirect tokens
+        if (strContains(tokens[tokenLength - 1], "<>") || strContains(tokens[0], "<>")) {
+            printf("Invalid redirection; need a file\n");
+            continue;
+        }
+
+        /* Handle built-in commands */
+        if (strcmp(tokens[0], "exit") == 0) {
+            freeTokens(tokens);
+            break;
+
+        } else if (strcmp(tokens[0], "h") == 0 || strcmp(tokens[0], "history") == 0) {
+            // Adding command to history before it is executed to show the correct recall order
+            addToCommandHistory(line);
+            showCommandHistory();
+
+        } else if (strcmp(tokens[0], "pwd") == 0) {
+            printPWD();
+            addToCommandHistory(line);
+
+        } else if (strcmp(tokens[0], "cd") == 0) {
+            // Change directory into arg1, else $HOME
+            if (chdir(tokens[1] ? tokens[1] : getenv("HOME")) == 0) {
+                addToCommandHistory(line);
+                printPWD();
+            } else {
+                perror(tokens[1]);
+            }
+
+        } else {
+            /* Handle programs */
+            char *exec;
+            if ((exec = findExecutable(tokens[0], path))) {
+                int stdin = STDIN_FILENO;
+                int stdout = STDOUT_FILENO;
+
+                /* Check for redirection */
+                if (tokenLength > 2 && strContains(tokens[tokenLength - 2], "<>")) {
+                    // stdin redirection
+                    if (strcmp(tokens[tokenLength - 2], "<") == 0 &&
+                        (stdin = open(tokens[tokenLength - 1], O_RDONLY)) == -1) {
+                        perror("Input redirection");
                         continue;
-                    } else if (tokenLength > 2 && strContains(tokens[tokenLength - 2], "<>")) {
-                        if (strcmp(tokens[tokenLength - 2], "<") == 0 &&
-                            (stdin = open(tokens[tokenLength - 1], O_RDONLY)) == -1) {
-                            printf("Could not redirect input from %s.\n", tokens[tokenLength - 1]);
-                            continue;
-                        };
+                    };
 
-                        if (strcmp(tokens[tokenLength - 2], ">") == 0 &&
-                            (stdout = open(tokens[tokenLength - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644)) == -1) {
-                            printf("Could not redirect output to %s.\n", tokens[tokenLength - 1]);
-                            continue;
-                        }
-
-                        if (strcmp(tokens[tokenLength - 2], ">>") == 0 &&
-                            (stdout = open(tokens[tokenLength - 1], O_WRONLY | O_APPEND | O_CREAT, 0644)) == -1) {
-                            printf("Could not redirect output to %s.\n", tokens[tokenLength - 1]);
-                            continue;
-                        }
-
-                        // Remove the redirect and path tokens as we don't want them to be passed to the program
-                        free(tokens[tokenLength - 1]);
-                        free(tokens[tokenLength - 2]);
-                        tokens[tokenLength - 2] = NULL;
+                    // stdout redirection
+                    if (strcmp(tokens[tokenLength - 2], ">") == 0 &&
+                        (stdout = open(tokens[tokenLength - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644)) == -1) {
+                        perror("Output redirection");
+                        continue;
                     }
 
-
-                    printf("Running %s ...\n", exec);
-                    if (stdout == STDOUT_FILENO) printf("--------------------\n");
-                    if ((pid = fork() != 0)) {
-                        wait(&stat);
-
-                        if (stdout == STDOUT_FILENO) printf("--------------------\n");
-                        printf("Returns %d\n", stat);
-                        addToCommandHistory(line);
-                    } else {
-                        // Child
-                        dup2(stdin, STDIN_FILENO);
-                        dup2(stdout, STDOUT_FILENO);
-                        execve(exec, tokens, envp);
-                        return -1;
+                    // stdout append redirection
+                    if (strcmp(tokens[tokenLength - 2], ">>") == 0 &&
+                        (stdout = open(tokens[tokenLength - 1], O_WRONLY | O_APPEND | O_CREAT, 0644)) == -1) {
+                        perror("Output redirection");
+                        continue;
                     }
-                } else {
-                    printf("Command not found.\n");
+
+                    // Remove the redirect and path tokens as we don't want them to be passed to the program
+                    free(tokens[tokenLength - 1]);
+                    free(tokens[tokenLength - 2]);
+                    tokens[tokenLength - 2] = NULL;
                 }
+
+                printf("Running %s ...\n", exec);
+                printf("--------------------\n");
+                if ((pid = fork() != 0)) {
+                    /* Parent Process */
+                    // Wait for child to exit, then print return code and add the command to the command history
+                    wait(&stat);
+                    printf("--------------------\n");
+                    printf("Returns %d\n", stat);
+                    addToCommandHistory(line);
+                } else {
+                    /* Child Process */
+                    // Redirect stdin and stdout if needed and transform process
+                    if (stdin != STDIN_FILENO) dup2(stdin, STDIN_FILENO);
+                    if (stdout != STDOUT_FILENO) dup2(stdout, STDOUT_FILENO);
+                    execve(exec, tokens, envp);
+                    return -1;
+                }
+            } else {
+                printf("%s: Command not found\n", tokens[0]);
             }
         }
     }
+
+    // No more commands, save, clean up and exit
     saveCommandHistory();
     cleanCommandHistory();
 
@@ -214,28 +237,36 @@ int main(int argc, char *argv[], char *envp[]) {
 // fileNameExpand: expand any wildcards in command-line args
 // - returns a possibly larger set of tokens
 char **fileNameExpand(char **tokens) {
+    // Count number of tokens
     int tokenLength = 0;
     while (tokens[++tokenLength] != NULL);
 
+    // Initialise an array of `glob_t` structures
     glob_t *globbuf = malloc(tokenLength * sizeof(glob_t));
 
+    // Resolve the wildcard expansions, and count the total number of tokens
     int newTokenLength = 0;
     for (int i = 0; i < tokenLength; i++) {
         glob(tokens[i], GLOB_NOCHECK | GLOB_TILDE, NULL, &globbuf[i]);
         newTokenLength += globbuf[i].gl_pathc;
     }
 
+    // Initialise a new array of tokens
     char **newTokens = malloc((newTokenLength + 1) * sizeof(char *));
+
+    // Populate the new token array
     int tokenCounter = 0;
     for (int i = 0; i < tokenLength; i++) {
         for (int j = 0; j < globbuf[i].gl_pathc; j++) {
             newTokens[tokenCounter++] = strdup(globbuf[i].gl_pathv[j]);
         }
     }
+
+    // NULL terminate the token array
     newTokens[tokenCounter] = NULL;
 
+    // Clean up
     for (int i = 0; i < tokenLength; i++) globfree(&globbuf[i]);
-
     free(globbuf);
     freeTokens(tokens);
 
@@ -268,21 +299,16 @@ char *findExecutable(char *cmd, char **path) {
 int isExecutable(char *cmd) {
     struct stat s;
     // must be accessible
-    if (stat(cmd, &s) < 0)
-    return 0;
+    if (stat(cmd, &s) < 0) return 0;
     // must be a regular file
     //if (!(s.st_mode & S_IFREG))
-    if (!S_ISREG(s.st_mode))
-        return 0;
+    if (!S_ISREG(s.st_mode)) return 0;
     // if it's owner executable by us, ok
-    if (s.st_uid == getuid() && s.st_mode & S_IXUSR)
-        return 1;
+    if (s.st_uid == getuid() && s.st_mode & S_IXUSR) return 1;
     // if it's group executable by us, ok
-    if (s.st_gid == getgid() && s.st_mode & S_IXGRP)
-        return 1;
+    if (s.st_gid == getgid() && s.st_mode & S_IXGRP) return 1;
     // if it's other executable by us, ok
-    if (s.st_mode & S_IXOTH)
-        return 1;
+    if (s.st_mode & S_IXOTH) return 1;
     return 0;
 }
 
